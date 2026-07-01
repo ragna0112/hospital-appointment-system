51f4f18601c448b6c918281afa391fa05ad2492e
Rich Daniel Lim <rich@Richs-MacBook-Air.local>
2026-07-02 01:10:56 +0800

    feat: setup React Router and home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <main>
-      <h1>🏥 Hospital Appointment System</h1>
-
-      <p>
-        Welcome! Our React application is running successfully.
-      </p>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <main>
+            <h1>🏥 Hospital Appointment System</h1>
+            <p>This is the Home Page.</p>
+        </main>
+    );
+}
+
+export default HomePage;
